ed2ea2e268682e6e02b9017763e282c2d08fe01b
Stjepan Poljak <stjepan.poljak@ericsson.com>
2020-05-08 00:15:59 +0200

    Python3 __getitem__ added.

--- python/data-model/data-model-ex.py
+++ python/data-model/data-model-ex.py
@@ -39,6 +39,8 @@ class RabuRettaRound():
             Card("Guard", 1, 5)
             ]
 
+    rounds_played = 0
+
     def __init__(self, players=4):
         """Rabu Retta round init"""
 
@@ -60,6 +62,25 @@ class RabuRettaRound():
         for card in self.deck:
             sum += card.count
         return sum
+
+    def __getitem__(self, position):
+        """for RabuRettaRound()[index]"""
+
+        length = len(self)
+
+        if 0 > position or position >= length:
+            raise IndexError
+
+        current = 0
+
+        for card in self.deck:
+
+            if position >= current and position <= current + card.count:
+                return card
+
+            current += card.count
+
+        raise IndexError
 
     def remove_card(self, to_remove):
         """remove card from the deck method"""
@@ -85,6 +106,10 @@ class RabuRettaRound():
 
             current += card.count
 
+    def __del__(self):
+        """Called when garbage collector calls destructor"""
+        RabuRettaRound.rounds_played += 1
+
 class RabuRettaRoundTests(unittest.TestCase):
 
     def test_players(self):
@@ -97,7 +122,7 @@ class RabuRettaRoundTests(unittest.TestCase):
                 try:
                     RabuRettaRound(num)
                 except Exception:
-                    self.fail("Should not raise exception")
+                    self.fail("Number of players should be valid.")
 
     def test_len(self):
         """Test deck size for various player number inputs."""
@@ -105,5 +130,29 @@ class RabuRettaRoundTests(unittest.TestCase):
         for _ in range(0, 100):
             for num in range(2, 5):
                 self.assertEqual(len(RabuRettaRound(num)), 12 if num == 2 else 15)
+
+    def test_getitem(self):
+        """Test __getitem__ method"""
+
+        for num in range(2,5):
+            rrr = RabuRettaRound(num)
+            rrr_length = len(rrr)
+
+            for index in range(-3, rrr_length + 3):
+
+                if index >= rrr_length or index < 0:
+                    with self.assertRaises(IndexError):
+                        rrr[index]
+                    # Note: self.assertRaises(IndexError, rrr[index])
+                    # won't work because rrr[index] will raise IndexError
+                    # before self.assertRaises gets called; another
+                    # solution would be to make rrr[index] a method, e.g.:
+                    # self.assertRaises(IndexError, lambda: rrr[index])
+                else:
+                    try:
+                        _ = rrr[index]
+                    except IndexError:
+                        self.fail("Index should not be out of bounds.")
+
 
 unittest.main()
