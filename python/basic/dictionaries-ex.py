3b386bd9f437eccc5ec8ab174aef6c8caa8316a5
ayush-programer <57296254+ayush-programer@users.noreply.github.com>
2020-09-30 21:41:29 +0530

    Update dictionaries-ex.py

--- python/basic/dictionaries-ex.py
+++ python/basic/dictionaries-ex.py
@@ -2,59 +2,65 @@
 
 empty_dictionary = {}
 
-print(empty_dictionary)
+print empty_dictionary
+
 # {}
 
 if empty_dictionary:
-    print("Not empty.")
+    print 'Not empty.'
 else:
-    print("Empty.")
+    print 'Empty.'
+
 # Empty.
 
-color = {
-    'Red': 127,
-    'Green': 32,
-    'Blue': 255
-}
+color = {'Red': 127, 'Green': 32, 'Blue': 255}
 
-print(color)
+print color
+
 # {'Red': 127, 'Green': 32, 'Blue': 255}
 
-print(color['Green'])
+print color['Green']
+
 # 32
 
 color['Alpha'] = 0.5
 
-print(color)
+print color
+
 # {'Red': 127, 'Green': 32, 'Blue': 255, 'Alpha': 0.5}
 
 color['Alpha'] = 1.0
 
-print(color)
+print color
+
 # {'Red': 127, 'Green': 32, 'Blue': 255, 'Alpha': 1.0}
 
 del color['Alpha']
 
-for key, value in color.items():
-    print(key + "\t-> " + str(value))
+for (key, value) in color.items():
+    print key + '\t-> ' + str(value)
+
 # Red       -> 127
 # Green     -> 32
 # Blue      -> 255
 
-val_string = "Values:"
+val_string = 'Values:'
 for key in color.keys():
-    val_string += " "
+    val_string += ' '
     val_string += str(color[key])
-print(val_string)
+print val_string
+
 # Values: 127 32 255
 
-print(sorted(color.values()))
+print sorted(color.values())
+
 # [32, 127, 255]
 
 if 'Alpha' not in color.keys():
-    print("No alpha!")
+    print 'No alpha!'
 else:
-    print("There is alpha.")
+    print 'There is alpha.'
+
 # No alpha!
 
 # Note: It is possible to store lists and
@@ -64,20 +70,20 @@ from collections import OrderedDict
 
 numbers_dict = OrderedDict()
 
-numbers_dict["One"] = 1
-numbers_dict["Two"] = 2
-numbers_dict["Three"] = 3
-numbers_dict["Zero"] = 0
+numbers_dict['One'] = 1
+numbers_dict['Two'] = 2
+numbers_dict['Three'] = 3
+numbers_dict['Zero'] = 0
 
-print(
-        "OrderedDict keeps order of items as "
-        "they were added:"
-        )
+print 'OrderedDict keeps order of items as they were added:'
 
 for number in numbers_dict.keys():
-    print("\t* " + number + ": " + str(numbers_dict[number]))
+    print '\t* ' + number + ': ' + str(numbers_dict[number])
+
 # OrderedDict keeps order of items as they were added:
-#	* One: 1
-#	* Two: 2
-#	* Three: 3
+# ....* One: 1
+# ....* Two: 2
+# ....* Three: 3
 #       * Zero: 0
+
+			
